eaceb2110c25ddf83f98cd40d127ba1e78916e88
jmleaux <chip@jmlowe.net>
2017-06-02 16:32:00 +0300

    finished lecture 5/87

---
+++ Pokedex3/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  Pokedex3
+//
+//  Created by J. M. Lowe on 6/2/17.
+//  Copyright © 2017 JMLeaux LLC. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
